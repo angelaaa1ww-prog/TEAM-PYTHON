507f619a30c394cbfdbc462ce022e4997fe14068
angelaaa1ww-prog <angelaaa1ww@gmail.com>
2026-02-18 10:54:41 +0300

    student_details

---
+++ student_details.py
@@ -0,0 +1,10 @@
+# Name : Angela Amani
+# Date : 17//02/2026
+#Program to display student details
+
+name = "Angela Amani" 
+sport = "Football" 
+grade = "A"
+hobby = "Painting"
+course = "Computer Science"
+print(f"My name is {name}. I play {sport} and I got a grade of {grade} in my last exam. My hobby is {hobby} and I am currently studying {course}.")
